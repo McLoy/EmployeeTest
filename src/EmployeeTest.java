fbc543eba3da8cbf83952f4b29b6afc9a228fff6
McLoy <tkachenko.vlad@hotmail.com>
2016-09-02 21:55:06 +0300

    update

--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -10,7 +10,7 @@ public class EmployeeTest {
         staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
         staff[2] = new Employee("Tony Tester", 40000, 1990, 3, 15);
 
-        for (Employee e:staff)
+        for (Employee e : staff)
             e.raiseSalary(5);
 
         for (Employee e : staff)
